5cf73b09ee8c56a0d495ef7df2974b54f611be89
kongnamul-muchim <rlaehdduf04@gmail.com>
2026-04-23 17:35:51 +0900

    Fix: IEnemy.Update에 UpdateViewDirection 누락 추가 (MonoBehaviour.Update와 동작 통일)

--- Assets/Core/Enemy/EnemyAIController.cs
+++ Assets/Core/Enemy/EnemyAIController.cs
@@ -359,12 +359,14 @@ namespace HideAndInk.Core.Enemy
 
         /// <summary>
         /// IEnemy.Update 구현
+        /// MonoBehaviour.Update와 동일한 동작 보장
         /// </summary>
         public void Update(float deltaTime)
         {
             if (!_isActive) return;
             UpdateAI(deltaTime);
             UpdateMovement(deltaTime);
+            UpdateViewDirection();
         }
     }
 }
